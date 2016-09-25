3b0aab4d5882711b15e823c7fced508580c37728
tconcannon <taycon@seas.upenn.edu>
2016-09-24 20:38:42 -0400

    first push

---
+++ guitarHero/game.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var GuitarHero=React.createClass({
+	render: function(){
+		return(<div>Testing</div>)
+	}
+})
+module.exports=GuitarHero
---
+++ guitarHero/routes/game.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <h1>testing</h1>,
+  document.getElementById('example')
+);
